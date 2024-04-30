4f1d5e2cd898d64885eae2ccf18d111d285ffe33
chandra.ganivada <chandra.ganivada@intellectdesign.com>
2024-04-30 15:22:25 +0530

    removed unwanted code

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -29,24 +29,6 @@ const connectDB = async () => {
 
 connectDB();
 
-mongoose.connection.on("error", () => {
-  console.log("Database connection error");
-});
-
-mongoose.connection.on("open", () => {
-  console.log("Database Connection is open");
-});
-
-api.use((req, res, next) => {
-  const error = new Error("Something went wrong");
-  next(error);
-});
-
-// Error-handling Middleware
-api.use((err, req, res, next) => {
-  console.error("Error:", err.message);
-  res.status(500).send("Internal Server Error");
-});
 
 api.listen(process.env.PORT, () => {
   console.log(`App running on port ${process.env.PORT}.`);
